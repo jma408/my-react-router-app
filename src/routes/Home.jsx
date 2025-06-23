01d3463c2abfa7850a1ea938df8a9bc80570459f
jma408 <jma408@gmail.com>
2025-06-23 14:01:08 -0700

    more

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -9,8 +9,8 @@ export function homeLoader() {
 export default function Home() {
   const post = useLoaderData();
   return (
-    <div>
-      <h2>Title:{post.title}</h2>
+    <div style={{ textAlign: "center", marginTop: "2rem" }}>
+      <h2 className="fancy-heading">Title:{post.title}</h2>
       <p>Body: {post.body}</p>
     </div>
   );
